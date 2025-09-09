30b5f15890cfa21ddf8fe82ffd4cfea2927a340a
Kribbany <palyi.kristof@gmail.com>
2025-09-09 13:03:35 +0200

    feat: add DotPattern component for animated background, update styles and dependencies in package.json and index.css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/header/header";
+import { DotPattern } from "@/components/magicui/dot-pattern";
+
 export default function App() {
   return (
     <div className="min-h-full text-offwhite">
       <Header></Header>
       <main className="px-6">
         <Outlet />
+        <div
+          className="w-full h-[100vh] absolute -z-1 top-0 left-0 bg-app"
+          id="dot-pattern"
+        >
+          <DotPattern className="w-full h-[100vh] opacity-20"></DotPattern>
+        </div>
       </main>
     </div>
   );
